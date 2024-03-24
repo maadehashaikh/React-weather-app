a0d99be5266cdf406afe77a5ae813b9a9650cdcb
Maadeha shaikh <maadeham@gmail.com>
2024-03-24 16:46:26 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Temprature from './components/weather/temprature';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Temprature/>
+    </>
+  );
+}
+
+export default App;
