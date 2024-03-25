4c342df530451bbc2f1dbf47ad4e658608231eda
Maadeha shaikh <maadeham@gmail.com>
2024-03-26 04:12:09 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import Temprature from './components/weather/temprature';
+
 import './App.css';
 
 function App() {
--- src/components/weather/temprature.js
+++ src/components/weather/temprature.js
@@ -1,58 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css'
+import Weathercart from './weathercart';
 const Temprature = () => {
+  const [searchValue , setSearchValue] = useState("Kotri");
+  const [tempInfo,setTempInfo] = useState({});
+
+  const getWeatherInfo = async ()=>{
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=7c389f6914a6d7b34940ea543318648b`
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      const {temp ,humidity ,pressure}= data.main;
+      const {main:weathermood} = data.weather[0];
+      const {name} = data;
+      const {speed} = data.wind;
+      const {country , sunset} = data.sys;
+      const myNewWeatherInfo = {
+       temp,humidity,pressure ,weathermood,speed ,country ,sunset
+      }
+      setTempInfo(myNewWeatherInfo);
+
+    } catch (error) {
+       console.log(error);
+    }
+  }
+    useEffect(() => {
+      getWeatherInfo();
+    },[])
+  
   return (
     <>
     <div className='wrap'>
       <div className='search'>
-       <input type='search' placeholder="search..." id='search' className='searchTerm'/>
-       <button className='searchButton' type='button'>Search </button>
+       <input type='search' placeholder="search..." id='search' className='searchTerm' value={searchValue} 
+       onChange={(e)=>setSearchValue(e.target.value)}/>
+       <button className='searchButton' type='button' onClick={getWeatherInfo}>Search </button>
        </div>
       </div>
       {/* Our tempearture card  */}
-      <article className='widget'>
-        <div className='weatherIcon'>
-          <i className={"wi wi-day-sunny"}></i>
-        </div>
-        <div className='weatherInfo'>
-       <div className='temperature'>
-        <span>25.5°deg</span>
-       </div>
-       <div className='description'>
-        <div className='weatherCondition'>Cloudy</div>
-        <div className='place'>Kotri ,Pakistan </div>
-       </div>
-        </div>
-        <div className='date'>{new Date().toLocaleString()}</div>
-
-        {/* Our 4 column sections  */}
-        <div className='extra-temp'>
-        <div className='temp-info-minmax'>
-
-          <div className='two-sided-section'>
-            <p><i className={"wi wi-sunset"}></i></p>
-            <p className='extra-info-leftside'>19:20 PM <br/>Sunset</p>
-          </div>
-
-          <div className='two-sided-section'>
-            <p><i className={"wi wi-humidity"}></i></p>
-            <p className='extra-info-leftside'>05:00 PM <br/>humidity</p>
-          </div>
-          </div>
-          <div className='weather-extra-info'>
-          <div className='two-sided-section'>
-            <p><i className={"wi wi-rain"}></i></p>
-            <p className='extra-info-leftside'>12:50 AM <br/>Pressure</p>
-          </div>
-
-          <div className='two-sided-section'>
-            <p><i className={"wi wi-strong-wind"}></i></p>
-            <p className='extra-info-leftside'>8:00 AM <br/>Speed</p>
-          </div>
-
-          </div>
-        </div>
-      </article>
+      <Weathercart tempInfo = {tempInfo}/>
     </>
     
   )
